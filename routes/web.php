776b15745603fe8fee74df75f3d21726fe0fc5f1
IVAN GONZALEZ <RIGCSYSTEMS@users.noreply.github.com>
2024-09-23 16:30:20 -0600

    Primer commit, primera modificación de routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Bienvenido a DATAROA";
+
 });
+
+Route::get('/clientes', function () {
+    return "Bienvenido a la página de clientes";
+
+    });
+
+route::get('/clientes/{id}', function ($id) {
+    return "Bienvenido a la página de clientes con ID {$id}";
+
+    });
+
+route::get('/clientes/creacion', function () {
+    return "Bienvenido a la página de creación de clientes ROA";
+
+    });
+
+    route::get('/clientes/edicion', function () {
+        return "Bienvenido a la página de edicion de clientes ROA";
+    
+        });
+
+
+
