ed13d78003dd6a24e9c741dbf66e76d44f04b6ec
RUBEN IVAN GONZALEZ CARDOSO <172548824+RIGCSYSTEMS@users.noreply.github.com>
2024-10-29 20:53:24 -0400

    Creacion seeders y factories

---
+++ database/seeders/documentoseeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class documentoseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
